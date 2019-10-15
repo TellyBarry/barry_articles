e70b169503835c7bf4b4755eb81cd8dd7b1c19e6
Telly <tellybary99@gmail.com>
2019-10-15 12:03:49 +0200

    dernier commit

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\AdminType;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdController extends Controller
 {
@@ -40,5 +43,84 @@ class AdController extends Controller
     public function show(Article $ad){
         return $this->render('ad/show.html.twig',[
         'ad' => $ad]);
+    }
+
+
+
+       /**
+     * Permet d'afficher le formulaire d'edition
+     * 
+     * 
+     * @Route("/ads/{id}/edit", name="ads_edit" )
+     * @return Response
+     */
+    public function edit(Article $ad, Request $request){
+        $form = $this->createForm(AdminType::class, $ad);
+        $form->handleRequest($request);
+        return $this->render('ad/edit.html.twig',[
+            'form'=> $form->createView(),
+            'ad'=>$ad
+        ]);
+    }
+    
+
+    /**
+     * Tableau liste articles
+     * 
+     * @Route("/admin", name="articles_admin")
+     * 
+     */
+    public function TableauAdmin(ArticleRepository $repo)
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/admin.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Suprimer 
+     * 
+     * 
+     */
+    
+
+     /**
+     * Creer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request, ObjectManager $manager){
+        $ad = new Article();
+    
+        $form = $this->createForm(AdminType::class, $ad);
+
+        $form->handleRequest($request);
+    
+        
+        if($form->isSubmitted()&& $form->isValid()){
+            foreach($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+            
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad -> getSlug()
+                ]);
+            }
+
+        return $this->render('ad/new.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
